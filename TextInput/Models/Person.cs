1ba7c623e4b7b4f39f7938dd6dc5f788a75696dc
Mikhail <143435410+mikhailSokolovskiy@users.noreply.github.com>
2023-11-13 15:22:37 +0500

    Dynamicly creating elements after writing in fields

---
+++ TextInput/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using Tmds.DBus.Protocol;
+
+namespace TextInput.Models;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string SecondName{ get; set; }
+    public string Adress { get; set; }
+    
+
+    public Person(string firstName, string secondName, string adress)
+    {
+        FirstName = firstName;
+        SecondName = secondName;
+        Adress = adress;
+    }
+}
--- TextInput/ViewModels/MainWindowViewModel.cs
+++ TextInput/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,11 @@
-﻿using System.Net.Mime;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Mime;
 using System.Reactive;
+using DynamicData.Kernel;
 using ReactiveUI;
+using TextInput.Models;
 
 namespace TextInput.ViewModels;
 
@@ -9,24 +14,26 @@ public class MainWindowViewModel : ViewModelBase
     private string _firstName;
     private string _secondName;
     private string _adress;
-    private int _repeat;
-    
+    private int _repeat = 1;
+
     public string FirstName
     {
         get => _firstName;
         set => this.RaiseAndSetIfChanged(ref _firstName, value);
     }
+
     public string SecondName
     {
         get => _secondName;
         set => this.RaiseAndSetIfChanged(ref _secondName, value);
     }
-    
+
     public string Adress
     {
         get => _adress;
         set => this.RaiseAndSetIfChanged(ref _adress, value);
     }
+
     public int Repeat
     {
         get => _repeat;
@@ -40,15 +47,20 @@ public class MainWindowViewModel : ViewModelBase
         get => _allInfo;
         set => this.RaiseAndSetIfChanged(ref _allInfo, value);
     }
+
     public ReactiveCommand<Unit, Unit> WriteText { get; }
+    public ObservableCollection<Person> PersonList { get; set; }
 
     public MainWindowViewModel()
     {
+        PersonList = new ObservableCollection<Person>(new List<Person>());
+
         WriteText = ReactiveCommand.Create((() =>
         {
+            if (Repeat == 0) Repeat = 1;
             for (int i = 0; i < Repeat; i++)
             {
-                AllInfo += FirstName + "\n" + SecondName + "\n" + Adress + "\n";
+                PersonList.Add(new Person(i+FirstName, SecondName, Adress));
             }
         }));
     }
