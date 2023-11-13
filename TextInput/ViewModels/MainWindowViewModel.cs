128869dbba5a9bdb0ad372581acbd12bf3b772d2
Mikhail <143435410+mikhailSokolovskiy@users.noreply.github.com>
2023-11-13 13:47:08 +0500

    simple text change

--- TextInput/ViewModels/MainWindowViewModel.cs
+++ TextInput/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace TextInput.ViewModels;
+﻿using ReactiveUI;
+
+namespace TextInput.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-  
+    private string _caption;
+
+    public string Caption
+    {
+        get => _caption;
+        set => this.RaiseAndSetIfChanged(ref _caption, value);
+    }
 }
