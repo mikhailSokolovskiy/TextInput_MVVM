e63e476d8cec6fe6950d3385b435156764675344
Mikhail <143435410+mikhailSokolovskiy@users.noreply.github.com>
2023-11-13 14:27:17 +0500

    Adding text after btn click

--- TextInput/ViewModels/MainWindowViewModel.cs
+++ TextInput/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,33 @@
-﻿using ReactiveUI;
+﻿using System.Net.Mime;
+using System.Reactive;
+using ReactiveUI;
 
 namespace TextInput.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
     private string _caption;
-
+    
     public string Caption
     {
         get => _caption;
         set => this.RaiseAndSetIfChanged(ref _caption, value);
+    }
+
+    private string _newText;
+
+    public string NewText
+    {
+        get => _newText;
+        set => this.RaiseAndSetIfChanged(ref _newText, value);
+    }
+    public ReactiveCommand<Unit, Unit> WriteText { get; }
+
+    public MainWindowViewModel()
+    {
+        WriteText = ReactiveCommand.Create((() =>
+        {
+            NewText = Caption;
+        }));
     }
 }
